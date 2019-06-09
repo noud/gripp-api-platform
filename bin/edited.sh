6593416da9513ba5ffa5584a1dab879657e97868
Noud de Brouwer <noud4@home.nl>
2019-06-09 12:21:28 +0000

    Edited script.

--- bin/edited.sh
+++ bin/edited.sh
@@ -5,3 +5,4 @@ cp src/Admin/MedewerkerAdmin.php.dist src/Admin/MedewerkerAdmin.php
 cp src/Admin/TagAdmin.php.dist src/Admin/TagAdmin.php
 cp src/Entity/Medewerker.php.dist src/Entity/Medewerker.php
 cp src/Entity/Taaktype.php.dist src/Entity/Taaktype.php
+bin/console cache:clear
