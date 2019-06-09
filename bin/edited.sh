7cab2c7aa9db4c1fc73e70bc0ce50a2c219f85be
Noud de Brouwer <noud4@home.nl>
2019-06-09 21:15:42 +0000

    Date, datetime and time form fields.

--- bin/edited.sh
+++ bin/edited.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env sh
+cp src/Admin/BedrijfAdmin.php.dist src/Admin/BedrijfAdmin.php
 cp src/Admin/BetalingAdmin.php.dist src/Admin/BetalingAdmin.php
-cp src/Admin/OfferteAdmin.php.dist src/Admin/OffertefaseAdmin.php
+cp src/Admin/ContactpersoonAdmin.php.dist src/Admin/ContactpersoonAdmin.php
+cp src/Admin/ContractAdmin.php.dist src/Admin/ContractAdmin.php
+cp src/Admin/FactuurAdmin.php.dist src/Admin/FactuurAdmin.php
+cp src/Admin/InkoopbetalingAdmin.php.dist src/Admin/InkoopbetalingAdmin.php
+cp src/Admin/InkoopfactuurAdmin.php.dist src/Admin/InkoopfactuurAdmin.php
+cp src/Admin/InkoopopdrachtAdmin.php.dist src/Admin/InkoopopdrachtAdmin.php
+cp src/Admin/MijnurenAdmin.php.dist src/Admin/MijnurenAdmin.php
+cp src/Admin/OfferteAdmin.php.dist src/Admin/OfferteAdmin.php
+cp src/Admin/OpdrachtAdmin.php.dist src/Admin/OpdrachtAdmin.php
 cp src/Admin/OpdrachtfaseAdmin.php.dist src/Admin/OpdrachtfaseAdmin.php
+cp src/Admin/TaakAdmin.php.dist src/Admin/TaakAdmin.php
 cp src/Admin/TaakfaseAdmin.php.dist src/Admin/TaakfaseAdmin.php
 cp src/Admin/TaaktypeAdmin.php.dist src/Admin/TaaktypeAdmin.php
+cp src/Admin/MedewerkerDatumAdmin.php.dist src/Admin/MedewerkerDatumAdmin.php
 cp src/Admin/MedewerkerAdmin.php.dist src/Admin/MedewerkerAdmin.php
 cp src/Admin/TagAdmin.php.dist src/Admin/TagAdmin.php
+cp src/Admin/TimelineAdmin.php.dist src/Admin/TimelineAdmin.php
+cp src/Admin/VerlofmutatieAdmin.php.dist src/Admin/VerlofmutatieAdmin.php
 cp src/Entity/Medewerker.php.dist src/Entity/Medewerker.php
 cp src/Entity/Taaktype.php.dist src/Entity/Taaktype.php
 bin/console cache:clear
