eb6d1687bc10333606078d10a942aef1ff0ca332
Noud de Brouwer <noud4@home.nl>
2019-06-09 14:02:30 +0200

    Edited script.

---
+++ bin/edited.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+cp src/Admin/BetalingAdmin.php.dist src/Admin/BetalingAdmin.php
+cp src/Admin/TaaktypeAdmin.php.dist src/Admin/TaaktypeAdmin.php
+cp src/Admin/MedewerkerAdmin.php.dist src/Admin/MedewerkerAdmin.php
+cp src/Admin/TagAdmin.php.dist src/Admin/TagAdmin.php
+cp src/Entity/Medewerker.php.dist src/Entity/Medewerker.php
+cp src/Entity/Taaktype.php.dist src/Entity/Taaktype.php
