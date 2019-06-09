6fea7ab99cc3d631fe26424d39c733e9ca3eb77e
Noud de Brouwer <noud4@home.nl>
2019-06-09 12:22:02 +0000

    Deactivate MedewerkerSalutationType.

--- src/Type/MedewerkerSalutationType.php
+++ src/Type/MedewerkerSalutationType.php
@@ -3,7 +3,10 @@
 namespace App\Type;
 
 use Sonata\Form\Type\BaseStatusType;
+use Symfony\Component\Form\FormTypeInterface;
 
-class MedewerkerSalutationType //extends BaseStatusType
+class MedewerkerSalutationType
+// extends BaseStatusType
+// implements FormTypeInterface
 {}
 //class MedewerkerSalutationType extends BaseType // BaseStatusType
