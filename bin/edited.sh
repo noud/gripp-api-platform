789bf0f41792e04c6d4bd2929c1b85332e26b0c6
Noud de Brouwer <noud4@home.nl>
2019-06-18 17:57:42 +0200

    Taakfase::__toString.

--- bin/edited.sh
+++ bin/edited.sh
@@ -4,5 +4,6 @@ cp src/Admin/PakketAdmin.php.dist src/Admin/PakketAdmin.php
 cp src/Admin/MedewerkerAdmin.php.dist src/Admin/MedewerkerAdmin.php
 cp src/Entity/Medewerker.php.dist src/Entity/Medewerker.php
 cp src/Entity/Taak.php.dist src/Entity/Taak.php
+cp src/Entity/Taakfase.php.dist src/Entity/Taakfase.php
 cp src/Entity/Timelineentry.php.dist src/Entity/Timelineentry.php
 bin/console cache:clear
