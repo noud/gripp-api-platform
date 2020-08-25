545a579c710e1245bd3f59d845a69152fe32c87d
Noud de Brouwer <noud4@home.nl>
2020-08-25 21:50:52 +0200

    timeline

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -1,19 +1,19 @@
 import 'vis-timeline/styles/vis-timeline-graph2d.min.css';
 import { Timeline } from "vis-timeline";
+var timeline = null;
 $( document ).ready(function() {
-    console.log('timeline.start1');
     var container = document.getElementById('visualization');
     if (container != null) {
         var drawn = $('#visualization').data('drawn');
         if (false === drawn) {
-            console.log('drawn',drawn);
-            document.getElementById('visualization').setAttribute('data', "drawn: 'true'");
-            document.getElementById('visualization').dataset.drawn = "true";
-            var items = $('#visualization').data('data-set');
-            console.log('items',items);
+            container.dataset.drawn = true;
+            var items = $('#visualization').data('dataset');
+            container.dataset.dataset = [];
             var options = {};    // @TODO how to make the timeline height and width dynamic?
-            console.log('timeline.start2');
-            new Timeline(container, items, options);
+            timeline = new Timeline(container, items, options);
+            if (container.childElementCount === 2) {
+                container.removeChild(container.lastElementChild);
+            }
         }
     }
 });
